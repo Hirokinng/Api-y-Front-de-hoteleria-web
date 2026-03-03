8cc4a40685e2085ce18d63c33092cc5c8d2dcfb5
charlielangumas <charlielangumasabreu@gmail.com>
2026-03-02 22:25:11 -0400

    Fix: EF Core packages + Program DI/DbContext

--- HoteleriaApp/Program.cs
+++ HoteleriaApp/Program.cs
@@ -1,16 +1,21 @@
 using HoteleriaApp.Infrastructure.Persistence.Contexto;
-using Microsoft.EntityFrameworkCore;
+using HoteleriaApp.Infrastructure.Persistence.Repositories;
+using HoteleriaApp.Infrastructure.Shared.Services;
+using HoteleriaApp.Core.Application.Interfaces;
 using HoteleriaApp.Core.Application.Services;
 using HoteleriaApp.Core.Domain.Interfaces;
-using HoteleriaApp.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using HoteleriaApp.Infrastructure.Persistence.Contexts;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 // Add services to the container.
 builder.Services.AddScoped<IPisoService, PisoService>();
 builder.Services.AddScoped<IPisoRepository, PisoRepository>();
-builder.Services.AddControllersWithViews();
+
 
 
 builder.Services.AddScoped<IClienteRepositorio, ClienteRepositorio>();
@@ -19,6 +24,9 @@ builder.Services.AddSingleton<IEmailServicio, EmailServicio>();
 
 
 builder.Services.AddDbContext<HoteleriaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
