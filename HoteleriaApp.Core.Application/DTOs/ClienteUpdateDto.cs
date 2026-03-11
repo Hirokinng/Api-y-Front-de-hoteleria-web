72cc8a3a6a12d592f640e04fe4fee274dd0fe219
Joslam Jerez Perez <20240266@itla.edu.do>
2026-03-11 16:18:12 -0400

    Reapply "Resolver conflictos aceptando develop"
    This reverts commit 9c3f18ba4926202c7f57209985c0daba8e08aff5.

---
+++ HoteleriaApp.Core.Application/DTOs/ClienteUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoteleriaApp.Core.Application.DTOs
+{
+    public class ClienteUpdateDto
+    {
+        public int id_cliente { get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string? telefono { get; set; }
+    }
+}
