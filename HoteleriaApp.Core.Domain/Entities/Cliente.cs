9c3f18ba4926202c7f57209985c0daba8e08aff5
Joslam Jerez Perez <20240266@itla.edu.do>
2026-03-11 14:33:31 -0400

    Revert "Resolver conflictos aceptando develop"
    This reverts commit 046b14912368628de903004a88d6b898551b3eb0, reversing
    changes made to 036ff8247ebdbf12ba18a1eaf619c74211854642.

--- HoteleriaApp.Core.Domain/Entities/Cliente.cs
+++ HoteleriaApp.Core.Domain/Entities/Cliente.cs
@@ -1,15 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
 
-namespace HoteleriaApp.Core.Domain.Entities;
-
-public class Cliente
+public class Cliente : BaseEntity
 {
-    [Key]
-    public int id_cliente { get; set; }
-    public string nombre { get; set; } = string.Empty;
-    public string email { get; set; } = string.Empty;
-    public string? telefono { get; set; }
-    public string password_hash { get; set; } = string.Empty;
-    public DateTime fecha_registro { get; set; }
-    public bool activo { get; set; } = true;
+    public string Nombre { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Telefono { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateOnly FechaRegistro { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    public bool Activo { get; set; } = true;
+
+    public ICollection<Reserva> Reservas { get; set; } = [];
 }
