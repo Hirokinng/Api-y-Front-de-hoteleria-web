5a0c4e292d5d619f07e34cccf8cea88fcd32052a
Joslam Jerez Perez <20240266@itla.edu.do>
2026-03-12 18:28:13 -0400

    Eliminando mas del modulo de reservas

--- HoteleriaApp.Core.Domain/InterfacesRepositories/IReservaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public interface IReservaRepository : IGenericRepository<Reserva>
-{
-    Task<Reserva?> GetByNumeroReservaAsync(string numeroReserva);
-    Task<IReadOnlyList<Reserva>> GetByClienteAsync(int idCliente);
-    Task<IReadOnlyList<Reserva>> GetByEstadoAsync(EstadoReserva estado);
-    Task<bool> TieneConflictoFechasAsync(int idHabitacion, DateOnly fechaEntrada, DateOnly fechaSalida);
-}
