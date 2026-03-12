e100878f202f1ab9548972c0c488c578ecd99a51
Joslam Jerez Perez <20240266@itla.edu.do>
2026-03-12 18:09:27 -0400

    Elimine los archivos sobrantes, renombre un archivo

--- HoteleriaApp.Core.Application/DTOs/Servicio/ServicioDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class ServicioDto
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public string? Descripcion { get; set; }
-    public decimal Precio { get; set; }
-}
--- HoteleriaApp.Core.Application/DTOs/TarifaDto/TarifaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class TarifaDto
-{
-    public int Id { get; set; }
-    public string NombreCategoria { get; set; } = string.Empty;
-    public string Temporada { get; set; } = "Base";
-    public decimal PrecioNoche { get; set; }
-}
